008da49b0d98e9090d3a3f821b8efc16dfd4c4ea
alumno305ecb16 <scorpions130295@gmail.com>
2015-06-26 11:32:19 -0500

    devaceactivo

--- js/acciones.js
+++ js/acciones.js
@@ -1,6 +1,6 @@
 // JavaScript Document
 $(document).ready(function(e) {	
-//document.addEventListener("deviceready",function(){
+document.addEventListener("deviceready",function(){
 	var numaleatorio1=0;
 	var numaleatorio2=0;
 	
@@ -355,4 +355,4 @@ $(document).ready(function(e) {
 	})
 	
 }); 
-//});
+});
